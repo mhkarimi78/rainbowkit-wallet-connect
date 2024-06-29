a911116ee68504bd89e86e5b0e941364fce50b5e
mhkarimi78 <mahsa_karim16@yahoo.com>
2024-06-29 11:14:39 +0330

    initialized project

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# RainbowKit Wallet Connection Example 🌈
-
-Example application integrating wallet connection with [RainbowKit](https://www.rainbowkit.com).
-
-![Screenshot](src/lib/screenshot.png)
-
-## Running the application
-
-```bash
-yarn install
-yarn dev
-```
+# RainbowKit Wallet Connection 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,6 @@ export default function Home() {
         <h1 className="text-2xl tracking-[-1px] font-semibold text-blue-600 text-center md:text-5xl">
           RainbowKit Wallet Connection 🌈
         </h1>
-        <p className="text-gray-700 text-base text-center md:text-xl">
-          Example application integrating wallet connection with{" "}
-          <a href="https://www.rainbowkit.com/" target="_blank">
-            RainbowKit
-          </a>
-          .
-        </p>
       </div>
       <ConnectButton />
     </div>
